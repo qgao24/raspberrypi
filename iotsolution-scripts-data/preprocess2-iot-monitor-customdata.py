b88c087c2d8ef722ae9be5b02766da9de3cee730
qgao24 <qgao24@myseneca.ca>
2023-08-07 11:17:48 -0400

    modify

--- iotsolution-scripts-data/preprocess2-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess2-iot-monitor-customdata.py
@@ -138,8 +138,8 @@ def sendtransactiondata(maintopic,mainproducerid,VIPERPORT,index,preprocesstopic
       # You can preprocess with the following functions: MAX, MIN, SUM, AVG, COUNT, DIFF
       # here we will take max values of the arcturus-humidity, we will Diff arcturus-temperature, and average arcturus-Light_Intensity
       # NOTE: The number of process logic functions MUST match the streams - the operations will be applied in the same order
-     preprocesslogic='avg,avg'
-     #preprocesslogic='anomprob,trend,avg,min,max,diff,outliers,variance,varied,outliers2-5,anomprob2-5,anomprob3,gm,hm,trend,IQR,trimean'
+     #preprocesslogic='avg,avg'
+     preprocesslogic='anomprob,trend,avg,min,max,diff,outliers,variance,varied,outliers2-5,anomprob2-5,anomprob3,gm,hm,trend,IQR,trimean'
      #preprocesslogic='diff'
      preprocessconditions=''
     
